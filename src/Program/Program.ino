5f912fb21862c98be108daf7d7c5e7ffba7c35a1
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-11-10 10:11:34 +0000

    Low-level rewrite

--- src/Program/Program.ino
+++ src/Program/Program.ino
@@ -1,93 +1,25 @@
-#include "WiFi.h"
-#include "WifiConfig.h"
-#include "TCPClientConfig.h"
-#include "WiFiMulti.h"
-#include "string.h"
+#include <unistd.h> 
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-void(* resetclient) (void) = 0;
 
-TCPClientConfig tcpcliconf;
-WifiConfig wc;
-
-WiFiClient tcpclient;
-
-void wifiinit()
+int main(int argc, char *argv[])
 {
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(wc.SSID, wc.WifiPassword);
-
-  Serial.print("\nConnecting to specified network");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.print("\nConnected to network");
-  Serial.print("\nSSID: " + WiFi.SSID() + "\nDevice IP: " + WiFi.localIP());
-}
-
-void tcpclientinit()
-{
-
-  tcpclient.connect(tcpcliconf.host, tcpcliconf.port);
-
-  Serial.println("\nAttempting to connect to specified NET-RMSI server at: " + String(tcpcliconf.host));
-  while (!tcpclient.connected())
-  {
-
-    tcpclient.connect(tcpcliconf.host, tcpcliconf.port);
-    Serial.print("\nRetrying in 5 seconds");
-    delay(5000);
-
-  }
-  Serial.print("\nConnected to TCPServer");
-
-  tcpclient.println(tcpcliconf.controlledcli);
-
-  String recvidentif = tcpclient.readString();
-
-  if (recvidentif == "valid")
-  {
-    Serial.print("\nClient-Server versioning the same");
-
-  }
-  else if (recvidentif == "invalid")
-  {
-    Serial.print("\nDisconnecting from server");
-    Serial.println("\nCurrent controller version: " + tcpcliconf.controlledcli);
-    Serial.print("client-server versioning mismatch please update, ensure server and client are on the same version");
-    tcpclient.stop();
-    resetclient();
-  }
-
-
+  int server_sockfd;
+  struct sockaddr_in server_addr;
+  server_addr.sin_family=AF_INET;
 }
 
 void setup()
 {
 
-  Serial.begin(115200);
-  delay(10000);
-  wifiinit();
-  tcpclientinit();
-
-
 }
 
 void loop()
 {
-  int recv = tcpclient.read();
-
-  if (recv == 0)
-  {
-    /* Relay control code here */
-  }
-  else if (recv == 1)
-  {
-    /* Relay control code here */
-  }
 
 }
